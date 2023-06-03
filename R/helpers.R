ffe3f2eca2f81b9ed03791c021e76adfa6d8a4c7
Ian G <56713583+pseudonoma@users.noreply.github.com>
2023-06-03 10:01:29 +1000

    Fixed unfixed bug
    I didn't copy the fix over.

--- R/helpers.R
+++ R/helpers.R
@@ -197,8 +197,9 @@ auto_rename <- function(data, projectName, testNames, renameKey){
     # testData <- dataPackage[[project]][["processed_data"]]
 
     # construct vector for replacement
+    # note: subKey is df, but data is a tibble and must be indexed with [[.
     replacements <- subKey[ ,targetCol][match(data[[targetCol]], subKey[ ,2])] # new values
-    originals <- data[,targetCol][!is.na(replacements)] # old values
+    originals <- data[[targetCol]][!is.na(replacements)] # old values
 
     if(all(is.na(replacements))){
       # replacements are all NA, so all() returns TRUE
@@ -206,13 +207,12 @@ auto_rename <- function(data, projectName, testNames, renameKey){
     } else {
       # replace elements of testData$`targetCol` that match non-NA replacements
       data[[targetCol]][!is.na(replacements)] <- replacements[!is.na(replacements)]
-      # currently replaces even identical values; not sure if I want to restrict this to
-      # only cases where values in replacements don't match target.
 
       # report changes because of paranoia (maybe this should be a warning?)
       message(paste(unique(originals[!is.na(originals)]),
                     "replaced with", unique(replacements[!is.na(replacements)]), "\n"))
     }
+
   }
 
 
